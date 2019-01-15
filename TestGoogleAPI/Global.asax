6e9b232860984852e74708865b5f0f9bd4349f4f
DangerFatty <k23671254@gmail.com>
2019-01-15 14:50:59 +0800

    加入專案檔案。

---
+++ TestGoogleAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestGoogleAPI.MvcApplication" Language="C#" %>
---
+++ TestGoogleAPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestGoogleAPI.Startup))]
+namespace TestGoogleAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
