d7b0d15062aa35e09b8634192e519be76a3b60ec
DangerFatty <k23671254@gmail.com>
2019-01-18 13:30:32 +0800

    add GoogleDrive API

---
+++ TestGoogleAPI/Controllers/GoogleDriveController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestGoogleAPI.Controllers
+{
+    public class GoogleDriveController : Controller
+    {
+        // GET: GoogleDrive
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
